42eed8829470e87afa8c4dc1ba730adef41691dd
Lingyi_Xu <lingyi.xu129@gmail.com>
2024-05-02 16:22:30 -0500

    Added background for Splash and 404 pages

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import backgroundImage from '../images/main_background.jpeg'
 
 const SplashPage = () => {
     return (
-        <section className="hero is-fullheight-with-navbar">
-            <div className="hero-body">
-                <div className="container has-text-centered">
+        <section className="hero is-fullheight-with-navbar" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+            <div className="hero-body" style={{margin: 'auto'}}>
+                <div className="container has-text-centered" style={{ backgroundColor: 'rgba(255, 255, 255, 0.9)', borderRadius: '10px', padding: '50px' }}>
                     <h1 className="title is-1">
                         FoodieMate
                     </h1>
