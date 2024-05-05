f9f369b4b660651a0aab7ab9f351b32a134eb3fb
Lingyi_Xu <lingyi.xu129@gmail.com>
2024-05-05 01:10:32 -0500

    Updated user home CSS

--- src/components/UserHomePage.jsx
+++ src/components/UserHomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import backgroundImage from '../images/shoppinglist_background.jpeg';
 
 function UserHomePage() {
     const [plans, setPlans] = useState([
@@ -10,15 +11,18 @@ function UserHomePage() {
     const [showAddItemForm, setShowAddItemForm] = useState(false);
     const [activeDropdown, setActiveDropdown] = useState(null);
     const [editItem, setEditItem] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         fetchShoppingList();
     }, []);
 
     const fetchShoppingList = async () => {
+        setLoading(true);
         const response = await fetch('/api/shopping-list');
         const data = await response.json();
         setShoppingList(data);
+        setLoading(false);
     };
 
     const handleAddOrUpdateItem = async (e) => {
@@ -69,90 +73,100 @@ function UserHomePage() {
     };
 
     return (
-        <div className="container">
-            <div className="columns">
-                <div className="column">
-                    <h2 className="title is-3">Plans</h2>
-                    {plans.map((plan, index) => (
-                        <div key={index} className="box">
-                            <h3>{plan.day}</h3>
-                            <p>Breakfast: {plan.meals.breakfast}</p>
-                            <p>Lunch: {plan.meals.lunch}</p>
-                            <p>Dinner: {plan.meals.dinner}</p>
-                        </div>
-                    ))}
-                </div>
-                <div className="column">
-                    <div className="is-flex is-justify-content-space-between">
-                        <h2 className="title is-3">Shopping list</h2>
-                        <button className="button is-info is-light"
-                            style={{ marginLeft: 'auto', marginTop: '1vw', marginBottom: '1vw' }}
-                            onClick={() => { setEditItem(null); setShowAddItemForm(true); }}>
-                            ➕
-                        </button>
+        <section className="hero is-fullheight-with-navbar" style={{
+            backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.5)), url(${backgroundImage})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat'
+        }}>
+            <div className="container">
+                <div className="columns">
+                    <div className="column">
+                        <h2 className="title is-3 has-text-centered-touch" style={{ marginTop: '1vw', marginLeft: '0.5rem' }}>Plans</h2>
+                        {plans.map((plan, index) => (
+                            <div key={index} className="box" style={{ marginLeft: '0.5rem', marginRight: '0.5rem' }}>
+                                <h3 className="has-text-centered-touch">{plan.day}</h3>
+                                <p>Breakfast: {plan.meals.breakfast}</p>
+                                <p>Lunch: {plan.meals.lunch}</p>
+                                <p>Dinner: {plan.meals.dinner}</p>
+                            </div>
+                        ))}
                     </div>
-                    {showAddItemForm && (
-                        <div className="box">
-                            <form onSubmit={handleAddOrUpdateItem}>
-                                <div className="field is-grouped">
-                                    <p className="control is-expanded">
-                                        <input className="input" type="text" name="item" defaultValue={editItem ? editItem.item : ''} placeholder="Item" required />
-                                    </p>
-                                    <p className="control is-expanded">
-                                        <input className="input" type="text" name="quantity" defaultValue={editItem ? editItem.quantity : ''} placeholder="Quantity" required />
-                                    </p>
-                                    <p className="control">
-                                        <button className="button is-success" type="submit">{editItem ? 'Update' : 'Add'}</button>
-                                    </p>
-                                    <p className="control">
-                                        <button className="button is-light" type="button" onClick={() => { setEditItem(null); setShowAddItemForm(false); }}>Cancel</button>
-                                    </p>
-                                </div>
-                            </form>
+                    <div className="column">
+                        <div className="is-flex is-justify-content-space-between">
+                            <h2 className="title is-3 has-text-centered-touch" style={{ marginTop: '1vw', marginLeft: '0.5rem' }}>Shopping list</h2>
+                            <button className="button is-info is-light"
+                                style={{ marginLeft: 'auto', marginTop: '1vw', marginBottom: '1vw', marginRight: '0.5rem' }}
+                                onClick={() => { setEditItem(null); setShowAddItemForm(true); }}>
+                                ➕
+                            </button>
                         </div>
-                    )}
-                    <div className="columns is-multiline">
-                        {shoppingList.map((item, index) => (
-                            <div key={index} className="column is-half">
-                                <div className="box">
-                                    <div className="level">
-                                        <div className="level-left">
-                                            <div className="level-item">
-                                                <div style={{ flexGrow: 1, maxWidth: '12vw', textOverflow: 'ellipsis' }}>
-                                                    <p><strong>{item.item}</strong></p>
-                                                    <p>{item.quantity}</p>
-                                                </div>
+                        {loading ? <progress className="progress is-small is-primary" max="100">Loading</progress> : (
+                            <>
+                                {showAddItemForm && (
+                                    <div className="box">
+                                        <form onSubmit={handleAddOrUpdateItem}>
+                                            <div className="field is-grouped">
+                                                <p className="control is-expanded">
+                                                    <input className="input" type="text" name="item" defaultValue={editItem ? editItem.item : ''} placeholder="Item" required />
+                                                </p>
+                                                <p className="control is-expanded">
+                                                    <input className="input" type="text" name="quantity" defaultValue={editItem ? editItem.quantity : ''} placeholder="Quantity" required />
+                                                </p>
+                                                <p className="control">
+                                                    <button className="button is-success" type="submit">{editItem ? 'Update' : 'Add'}</button>
+                                                </p>
+                                                <p className="control">
+                                                    <button className="button is-light" type="button" onClick={() => { setEditItem(null); setShowAddItemForm(false); }}>Cancel</button>
+                                                </p>
                                             </div>
-                                        </div>
-                                        <div className="level-right">
-                                            <div className="level-item">
-                                                <div className={`dropdown is-right ${activeDropdown === index ? 'is-active' : ''} is-hoverable`}>
-                                                    <div className="dropdown-trigger">
-                                                        <button className="button" aria-haspopup="true" aria-controls={`dropdown-menu-${index}`}>
-                                                            <span>...</span>
-                                                        </button>
+                                        </form>
+                                    </div>
+                                )}
+                                <div className="columns is-multiline">
+                                    {shoppingList.map((item, index) => (
+                                        <div key={index} className="column is-half">
+                                            <div className="box" style={{ margin: '0.5rem' }}>
+                                                <div className="level">
+                                                    <div className="level-left">
+                                                        <div className="level-item">
+                                                            <div style={{ flexGrow: 1, maxWidth: '12vw', textOverflow: 'ellipsis' }}>
+                                                                <p><strong>{item.item}</strong></p>
+                                                                <p>{item.quantity}</p>
+                                                            </div>
+                                                        </div>
                                                     </div>
-                                                    <div className="dropdown-menu" id={`dropdown-menu-${index}`} role="menu">
-                                                        <div className="dropdown-content">
-                                                            <button className="button is-white dropdown-item" onClick={() => handleEdit(item)}>
-                                                                Edit
-                                                            </button>
-                                                            <button className="button is-white dropdown-item" onClick={() => handleDelete(item._id)}>
-                                                                Delete
-                                                            </button>
+                                                    <div className="level-right">
+                                                        <div className="level-item">
+                                                            <div className={`dropdown is-right ${activeDropdown === index ? 'is-active' : ''} is-hoverable`}>
+                                                                <div className="dropdown-trigger">
+                                                                    <button className="button" aria-haspopup="true" aria-controls={`dropdown-menu-${index}`}>
+                                                                        <span>...</span>
+                                                                    </button>
+                                                                </div>
+                                                                <div className="dropdown-menu" id={`dropdown-menu-${index}`} role="menu">
+                                                                    <div className="dropdown-content">
+                                                                        <button className="button is-white dropdown-item" onClick={() => handleEdit(item)}>
+                                                                            Edit
+                                                                        </button>
+                                                                        <button className="button is-white dropdown-item" onClick={() => handleDelete(item._id)}>
+                                                                            Delete
+                                                                        </button>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
+                                    ))}
                                 </div>
-                            </div>
-                        ))}
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 }
 
