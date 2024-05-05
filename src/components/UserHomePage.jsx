61c91d619699d653a69033822eae73e9b469bd2c
Lingyi_Xu <lingyi.xu129@gmail.com>
2024-05-05 18:02:40 -0500

    Reverse changes to userhome

--- src/components/UserHomePage.jsx
+++ src/components/UserHomePage.jsx
@@ -2,47 +2,21 @@ import React, { useState, useEffect } from 'react';
 import backgroundImage from '../images/shoppinglist_background.jpeg';
 
 function UserHomePage() {
-    const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    const initialPlans = weekDays.map(day => ({ day, meals: { breakfast: '...', lunch: '...', dinner: '...' } }));
-
-    const [plans, setPlans] = useState(initialPlans);
-    // const [plans, setPlans] = useState([
-    //     { day: 'Mon', meals: { breakfast: 'Waffles', lunch: 'Chicken Salad', dinner: '...' } },
-    //     { day: 'Thu', meals: { breakfast: '...', lunch: '...', dinner: '...' } },
-    //     { day: 'Wed', meals: { breakfast: '...', lunch: '...', dinner: '...' } },
-    // ]);
+    const [plans, setPlans] = useState([
+        { day: 'Today - Wed', meals: { breakfast: 'Waffles', lunch: 'Chicken Salad', dinner: '...' } },
+        { day: 'Tomorrow - Thu', meals: { breakfast: '...', lunch: '...', dinner: '...' } },
+        { day: 'Apr 29 - Mon', meals: { breakfast: '...', lunch: '...', dinner: '...' } },
+    ]);
     const [shoppingList, setShoppingList] = useState();
     const [showAddItemForm, setShowAddItemForm] = useState(false);
     const [activeDropdown, setActiveDropdown] = useState(null);
     const [editItem, setEditItem] = useState(null);
     const [loading, setLoading] = useState(false);
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [currentPlan, setCurrentPlan] = useState(null);
-
     useEffect(() => {
         fetchShoppingList();
     }, []);
 
-    const handleEditPlan = (plan) => {
-        setCurrentPlan(plan);
-        setIsModalOpen(true);
-    };
-
-    const handleDeletePlan = async (day) => {
-        try {
-            setLoading(true);
-            const response = await fetch(`/api/deleteMealPlan/${day}`, { method: 'DELETE' });
-            if (!response.ok) throw new Error('Failed to delete the plan');
-            setPlans(plans.map(plan => plan.day === day ? { ...plan, meals: { breakfast: '...', lunch: '...', dinner: '...' } } : plan));
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    //
     const fetchShoppingList = async () => {
         setLoading(true);
         const response = await fetch('/api/shopping-list');
@@ -82,13 +56,13 @@ function UserHomePage() {
         setShowAddItemForm(false);
     };
 
-    const handleEditItem = (item) => {
+    const handleEdit = (item) => {
         setEditItem(item);
         setShowAddItemForm(true);
         setActiveDropdown(null);
     };
 
-    const handleDeleteItem = async (id) => {
+    const handleDelete = async (id) => {
         const response = await fetch(`/api/shopping-list/${id}`, {
             method: 'DELETE'
         });
@@ -112,15 +86,12 @@ function UserHomePage() {
                 <div className="columns">
                     <div className="column">
                         <h2 className="title is-3 has-text-centered-touch" style={{ marginTop: '1vw', marginLeft: '0.5rem' }}>Plans</h2>
-                        <button className="button is-info" onClick={() => { }}>Add Plan</button>
                         {plans.map((plan, index) => (
-                            <div key={index} className="box">
-                                <h3 className="title is-4">{plan.day}</h3>
+                            <div key={index} className="box" style={{ marginLeft: '0.5rem', marginRight: '0.5rem' }}>
+                                <h3 className="has-text-centered-touch">{plan.day}</h3>
                                 <p>Breakfast: {plan.meals.breakfast}</p>
                                 <p>Lunch: {plan.meals.lunch}</p>
                                 <p>Dinner: {plan.meals.dinner}</p>
-                                <button onClick={() => handleEditPlan(plan.day)}>Edit</button>
-                                <button onClick={() => handleDeletePlan(plan.day)}>Delete</button>
                             </div>
                         ))}
                     </div>
@@ -178,10 +149,10 @@ function UserHomePage() {
                                                                 </div>
                                                                 <div className="dropdown-menu" id={`dropdown-menu-${index}`} role="menu">
                                                                     <div className="dropdown-content">
-                                                                        <button className="button is-white dropdown-item" onClick={() => handleEditItem(item)}>
+                                                                        <button className="button is-white dropdown-item" onClick={() => handleEdit(item)}>
                                                                             Edit
                                                                         </button>
-                                                                        <button className="button is-white dropdown-item" onClick={() => handleDeleteItem(item._id)}>
+                                                                        <button className="button is-white dropdown-item" onClick={() => handleDelete(item._id)}>
                                                                             Delete
                                                                         </button>
                                                                     </div>
